803c843d7560b5be33114389594ccb7f0a5fa5e4
Cody Vig <cody.cjv@gmail.com>
2022-12-23 19:17:30 -0800

    Moved movement logic into a separate method and added normalization.

--- IndecisiveBearGame/Assets/Scripts/Player.cs
+++ IndecisiveBearGame/Assets/Scripts/Player.cs
@@ -1,12 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
     float speed = 0.01f;
+    Vector2 position;
     GameObject[] walls;
+
+    // Start is called before the first frame update
     void Start()
     {
         walls = GameObject.FindGameObjectsWithTag("Wall");
@@ -15,21 +15,39 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovementHandler();
+        transform.position = position;
+    }
+
+    public void MovementHandler()
+    {
+        int upMoveCount = 0;
+        int rightMoveCount = 0;
+
         if (Input.GetKey("w"))
         {
-            transform.position += transform.up * speed;
+            upMoveCount += 1;
         }
         if (Input.GetKey("s"))
         {
-            transform.position -= transform.up * speed;
+            upMoveCount -= 1;
         }
         if (Input.GetKey("a"))
         {
-            transform.position -= transform.right * speed;
+            rightMoveCount -= 1;
         }
         if (Input.GetKey("d"))
         {
-            transform.position += transform.right * speed;
+            rightMoveCount += 1;
+        }
+
+        if (upMoveCount != 0 || rightMoveCount != 0)
+        {
+            var magnitude =
+                Mathf.Sqrt(rightMoveCount * rightMoveCount + upMoveCount * upMoveCount);
+            var resultant =
+                rightMoveCount * Vector2.right + upMoveCount * Vector2.up;
+            position += (resultant / magnitude) * speed;
         }
     }
 }
