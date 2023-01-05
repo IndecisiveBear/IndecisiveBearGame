7c4a97d80934baa41cca49db61b77a3c13d5d784
Cody Vig <cody.cjv@gmail.com>
2023-01-04 17:55:52 -0800

    C# style guide compliance.

--- IndecisiveBearGame/Assets/Scripts/Player.cs
+++ IndecisiveBearGame/Assets/Scripts/Player.cs
@@ -5,55 +5,55 @@ public class Player : MonoBehaviour
     const float WalkSpeed = 0.01f;
     public float PlayerSpeed = WalkSpeed;
     public bool InCollision = false;
-    public Vector2 position;
-    GameObject[] walls;
-    BoxCollider2D body;
+    public Vector2 Position;
+    GameObject[] Walls;
+    BoxCollider2D Body;
 
     void Start()
     {
-        walls = GameObject.FindGameObjectsWithTag("Wall");
-        body = gameObject.GetComponent<BoxCollider2D>();
+        Walls = GameObject.FindGameObjectsWithTag("Wall");
+        Body = gameObject.GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
-        position = transform.position;
+        Position = transform.position;
         Move();
-        transform.position = position;
+        transform.position = Position;
 
         if (Input.GetKey(KeyCode.Q)) // Warp to origin, for debugging 
         {
-            position = Vector2.zero;
-            transform.position = position;
+            Position = Vector2.zero;
+            transform.position = Position;
         }
     }
 
     /// <summary>
-    /// <c>Move</c> handles all player movement using `wasd` keys. If `Undo` is `true`, movement will be reversed.
+    /// <c>Move</c> handles all player movement using `wasd` keys.
     /// </summary>
     public void Move()
     {
-        int _upMoveCount = 0;
-        int _rightMoveCount = 0;
+        int upMoveCount = 0;
+        int rightMoveCount = 0;
 
         if (Input.GetKeyDown(KeyCode.LeftShift)) PlayerSpeed = (PlayerSpeed == WalkSpeed) ? 2 * WalkSpeed : WalkSpeed;
-        if (Input.GetKey(KeyCode.W)) _upMoveCount += 1;
-        if (Input.GetKey(KeyCode.S)) _upMoveCount -= 1;
-        if (Input.GetKey(KeyCode.A)) _rightMoveCount -= 1;
-        if (Input.GetKey(KeyCode.D)) _rightMoveCount += 1;
+        if (Input.GetKey(KeyCode.W)) upMoveCount += 1;
+        if (Input.GetKey(KeyCode.S)) upMoveCount -= 1;
+        if (Input.GetKey(KeyCode.A)) rightMoveCount -= 1;
+        if (Input.GetKey(KeyCode.D)) rightMoveCount += 1;
  
-        if (_upMoveCount != 0 || _rightMoveCount != 0)
+        if (upMoveCount != 0 || rightMoveCount != 0)
         {
-            PlayerCollision(body, walls);
-            var magnitude = Mathf.Sqrt(_rightMoveCount * _rightMoveCount + _upMoveCount * _upMoveCount);
-            var resultant = _rightMoveCount * Vector2.right + _upMoveCount * Vector2.up;
-            position += (resultant / magnitude) * PlayerSpeed;
+            PlayerCollision(Body, Walls);
+            var magnitude = Mathf.Sqrt(rightMoveCount * rightMoveCount + upMoveCount * upMoveCount);
+            var resultant = rightMoveCount * Vector2.right + upMoveCount * Vector2.up;
+            Position += (resultant / magnitude) * PlayerSpeed;
         }
     }
 
     /// <summary>
-    /// <c>DetectCollision</c> handles collision detection logic between two BoxColliders. It also warps
-    /// player to nearest acceptable location if they are inside of a wall.
+    /// <c>DetectCollision</c> handles collision detection logic between two BoxColliders. 
+    /// It also warps player to nearest acceptable location if they are inside of a wall.
     /// </summary>
     /// <returns>
     /// Whether or not a collision was detected.
@@ -71,29 +71,29 @@ public class Player : MonoBehaviour
                 Mathf.Abs(body1.bounds.center.y - body2.bounds.center.y) < 1.5f * body2.bounds.extents.y)
             {
                 // Colliding with right edge of body2
-                position.x = body2.bounds.center.x + body2.bounds.extents.x + body1.bounds.extents.x;
-                transform.position = position;
+                Position.x = body2.bounds.center.x + body2.bounds.extents.x + body1.bounds.extents.x;
+                transform.position = Position;
             }
             if (body1.bounds.center.x < body2.bounds.center.x && 
                 Mathf.Abs(body1.bounds.center.y - body2.bounds.center.y) < 1.5f * body2.bounds.extents.y)
             {
                 // Colliding with left edge of body2
-                position.x = body2.bounds.center.x - body2.bounds.extents.x - body1.bounds.extents.x;
-                transform.position = position;
+                Position.x = body2.bounds.center.x - body2.bounds.extents.x - body1.bounds.extents.x;
+                transform.position = Position;
             }
             if (body1.bounds.center.y > body2.bounds.center.y && 
                 Mathf.Abs(body1.bounds.center.x - body2.bounds.center.x) < 1.5f * body2.bounds.extents.x)
             {
                 // Colliding with top edge of body2
-                position.y = body2.bounds.center.y + body2.bounds.extents.y + body1.bounds.extents.y;
-                transform.position = position;
+                Position.y = body2.bounds.center.y + body2.bounds.extents.y + body1.bounds.extents.y;
+                transform.position = Position;
             }
             if (body1.bounds.center.y < body2.bounds.center.y && 
                 Mathf.Abs(body1.bounds.center.x - body2.bounds.center.x) < 1.5f * body2.bounds.extents.x)
             {
                 // Colliding with bottom edge of body2
-                position.y = body2.bounds.center.y - body2.bounds.extents.y - body1.bounds.extents.y;
-                transform.position = position;
+                Position.y = body2.bounds.center.y - body2.bounds.extents.y - body1.bounds.extents.y;
+                transform.position = Position;
             }
             return true;
         }
