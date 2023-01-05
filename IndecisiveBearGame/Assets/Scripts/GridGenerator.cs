689b86420ebe5a8e8d698efef002c582d990a5c2
Cody Vig <cody.cjv@gmail.com>
2023-01-05 00:55:07 -0800

    Made `Grid` three-dimensional to better handle layers of game objects.

--- IndecisiveBearGame/Assets/Scripts/GridGenerator.cs
+++ IndecisiveBearGame/Assets/Scripts/GridGenerator.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class GridGenerator : MonoBehaviour
 {
     public GameObject Player;
-    public GameObject Walls;
+    public GameObject Wall;
+    public GameObject Ramp;
     public float GridSize = 2f;
+    public int MaxObjectCount = 10;
     public string[,] GridString;
-    GameObject[,] Grid;
+    GameObject[,,] Grid;
     GameObject Objects;
     
 
@@ -17,74 +19,81 @@ public class GridGenerator : MonoBehaviour
 
     void Main()
     {
-        Grid = new GameObject[GridString.GetLength(0), GridString.GetLength(1)];
+        // Fix this MaxObjectCount in Grid.Item3. 
+        // I could loop over all ParseGridString(gridString[i, j])
+        // and set MaxObjectCount = max{ParseGridString(gridString[i, j])} and
+        // Grid = new GameObject[GridString.GetLength(0), GridString.GetLength(1), MaxObjectCount];
+        Grid = new GameObject[GridString.GetLength(0), GridString.GetLength(1), MaxObjectCount];
         SetGrid(Grid, GridString);
         InstantiateGrid(Grid);
     }
 
-    void SetGrid(GameObject[,] grid, string[,] gridString)
+    void SetGrid(GameObject[,,] grid, string[,] gridString)
     {
+        string[] objectList;
+        string item;
         for(int i = 0; i < gridString.GetLength(0); i++)
         {
             for(int j = 0; j < gridString.GetLength(1); j++)
             {
                 // Get the list of game objects to place on the scene at grid point (i,j).
-                string[] objectList = GridStringParser(gridString[i, j]);
-                foreach (string item in objectList)
-                { 
-                    if (item.ToUpper() == " ")
+                objectList = ParseGridString(gridString[i, j]);
+                for (int k = 0; k < MaxObjectCount; k++)
+                {
+                    try 
                     {
-                        grid[i, j] = null;
+                        item = objectList[k];
+                        if (item.ToUpper() == " ") grid[i, j, k] = null;
+                        if (item.ToUpper() == "P") grid[i, j, k] = Player;
+                        if (item.ToUpper() == "W") grid[i, j, k] = Wall;
+                        if (item.ToUpper() == "R") grid[i, j, k] = Ramp;
                     }
-                    if (item.ToUpper() == "P")
+                    catch(System.IndexOutOfRangeException)
                     {
-                        grid[i, j] = Player;
-                    }
-                    if (item.ToUpper() == "W")
-                    {
-                        // We need additonal structure. Should we make Grid a 3D array?
-                        // Grid[i, j, k] can be such that k denotes the kth item from 
-                        // the ground at location (i,j).
-                        grid[i, j] = Walls;
+                        // If objectList.Length < MaxObjectCount, fill the rest with null
+                        grid[i, j, k] = null;
                     }
                 }
             }
         }
     }
 
-    void InstantiateGrid(GameObject[,] grid)
+    void InstantiateGrid(GameObject[,,] grid)
     {
         for (int i = 0; i < grid.GetLength(0); i++)
         {
             for (int j = 0; j < grid.GetLength(1); j++)
             {
-
-                if (grid[i, j] is not null)
+                for (int k = 0; k < MaxObjectCount; k++)
                 {
-                     Objects = Instantiate(
-                        grid[i, j], 
-                        new Vector2(j * GridSize, (4 - i) * GridSize), 
-                        Quaternion.identity);
+                    if (grid[i, j, k] is not null)
+                    {
+                        Objects = Instantiate(
+                            grid[i, j, k], 
+                            new Vector2(j * GridSize, (4 - i) * GridSize), 
+                            Quaternion.identity
+                        );
+                    }
                 }
             }
         }
     }
 
     /// <summary>
-    /// <c>GridStringParser</c> parses string `text` into an array of strings
+    /// <c>ParseGridString</c> parses string `text` into an array of strings
     /// where the elements of the array are strings from `text` separated by `delimiter`.
     /// </summary>
-    string[] GridStringParser(string text, string delimiter = ":")
+    string[] ParseGridString(string text, string delimiter = ":")
     { 
         // Some expected outputs...
 
-        // GridStringParser("A:BC:D");
+        // ParseGridString("A:BC:D");
         // >>> string[] { "A", "BC", "D" };
 
-        // GridStringParser(":A:BC:D:");
+        // ParseGridString(":A:BC:D:");
         // >>> string[] { "A", "BC", "D" };
 
-        // GridStringParser("ABCD");
+        // ParseGridString("ABCD");
         // >>> string[] { "ABCD" };
 
         string[] returnString = new string[2]; // Boilder plate code.
