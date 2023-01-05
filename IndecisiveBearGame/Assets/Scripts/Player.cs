83530d9b494965af3db86713805408d693229976
Cody Vig <85211320+CodyVig@users.noreply.github.com>
2023-01-04 18:02:31 -0800

    Collision Physics (#34)
    * Added BoxCollider2D to Player and to Walls for collision implementation.
    
    * Wrote code to detect collisions and stop player from colliding with walls.
    
    * Added debugging code to warp player to scene origin if `Q` is pressed.
    
    * Fixed C# style guide compliance.

--- IndecisiveBearGame/Assets/Scripts/Player.cs
+++ IndecisiveBearGame/Assets/Scripts/Player.cs
@@ -2,57 +2,117 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    const float SPEED = 0.02f;
-    float speed = SPEED;
-    Vector2 position;
-    GameObject[] walls;
+    const float WalkSpeed = 0.01f;
+    public float PlayerSpeed = WalkSpeed;
+    public bool InCollision = false;
+    public Vector2 Position;
+    GameObject[] Walls;
+    BoxCollider2D Body;
 
-    // Start is called before the first frame update
     void Start()
     {
-        walls = GameObject.FindGameObjectsWithTag("Wall");
+        Walls = GameObject.FindGameObjectsWithTag("Wall");
+        Body = gameObject.GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        position = transform.position;
+        Position = transform.position;
         Move();
-        transform.position = position;
+        transform.position = Position;
+
+        if (Input.GetKey(KeyCode.Q)) // Warp to origin, for debugging 
+        {
+            Position = Vector2.zero;
+            transform.position = Position;
+        }
     }
 
+    /// <summary>
+    /// <c>Move</c> handles all player movement using `wasd` keys.
+    /// </summary>
     public void Move()
     {
         int upMoveCount = 0;
         int rightMoveCount = 0;
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            speed = (speed == SPEED) ? 2 * SPEED : SPEED;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            upMoveCount += 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            upMoveCount -= 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rightMoveCount -= 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rightMoveCount += 1;
-        }
+        if (Input.GetKeyDown(KeyCode.LeftShift)) PlayerSpeed = (PlayerSpeed == WalkSpeed) ? 2 * WalkSpeed : WalkSpeed;
+        if (Input.GetKey(KeyCode.W)) upMoveCount += 1;
+        if (Input.GetKey(KeyCode.S)) upMoveCount -= 1;
+        if (Input.GetKey(KeyCode.A)) rightMoveCount -= 1;
+        if (Input.GetKey(KeyCode.D)) rightMoveCount += 1;
+ 
         if (upMoveCount != 0 || rightMoveCount != 0)
         {
-            var magnitude =
-                Mathf.Sqrt(rightMoveCount * rightMoveCount + upMoveCount * upMoveCount);
-            var resultant =
-                rightMoveCount * Vector2.right + upMoveCount * Vector2.up;
-            position += (resultant / magnitude) * speed;
+            PlayerCollision(Body, Walls);
+            var magnitude = Mathf.Sqrt(rightMoveCount * rightMoveCount + upMoveCount * upMoveCount);
+            var resultant = rightMoveCount * Vector2.right + upMoveCount * Vector2.up;
+            Position += (resultant / magnitude) * PlayerSpeed;
         }
+    }
+
+    /// <summary>
+    /// <c>DetectCollision</c> handles collision detection logic between two BoxColliders. 
+    /// It also warps player to nearest acceptable location if they are inside of a wall.
+    /// </summary>
+    /// <returns>
+    /// Whether or not a collision was detected.
+    /// </returns>
+    private bool DetectCollision(BoxCollider2D body1, BoxCollider2D body2)
+    {
+
+        if (body1.bounds.center.x + body1.bounds.extents.x >= body2.bounds.center.x - body2.bounds.extents.x &&
+            body1.bounds.center.x - body1.bounds.extents.x <= body2.bounds.center.x + body2.bounds.extents.x &&
+            body1.bounds.center.y + body1.bounds.extents.y >= body2.bounds.center.y - body2.bounds.extents.y &&
+            body1.bounds.center.y - body1.bounds.extents.y <= body2.bounds.center.y + body2.bounds.extents.y)
+        {
+            // Detect the edge with which we are colliding.
+            if (body1.bounds.center.x > body2.bounds.center.x && 
+                Mathf.Abs(body1.bounds.center.y - body2.bounds.center.y) < 1.5f * body2.bounds.extents.y)
+            {
+                // Colliding with right edge of body2
+                Position.x = body2.bounds.center.x + body2.bounds.extents.x + body1.bounds.extents.x;
+                transform.position = Position;
+            }
+            if (body1.bounds.center.x < body2.bounds.center.x && 
+                Mathf.Abs(body1.bounds.center.y - body2.bounds.center.y) < 1.5f * body2.bounds.extents.y)
+            {
+                // Colliding with left edge of body2
+                Position.x = body2.bounds.center.x - body2.bounds.extents.x - body1.bounds.extents.x;
+                transform.position = Position;
+            }
+            if (body1.bounds.center.y > body2.bounds.center.y && 
+                Mathf.Abs(body1.bounds.center.x - body2.bounds.center.x) < 1.5f * body2.bounds.extents.x)
+            {
+                // Colliding with top edge of body2
+                Position.y = body2.bounds.center.y + body2.bounds.extents.y + body1.bounds.extents.y;
+                transform.position = Position;
+            }
+            if (body1.bounds.center.y < body2.bounds.center.y && 
+                Mathf.Abs(body1.bounds.center.x - body2.bounds.center.x) < 1.5f * body2.bounds.extents.x)
+            {
+                // Colliding with bottom edge of body2
+                Position.y = body2.bounds.center.y - body2.bounds.extents.y - body1.bounds.extents.y;
+                transform.position = Position;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// <c>PlayerCollision</c> sets `Player.InCollision` to `true` if player is inside one or more `obstacles`.
+    /// </summary>
+    private void PlayerCollision(BoxCollider2D playerBody, GameObject[] obstacles)
+    { 
+        foreach (GameObject obstacle in obstacles)
+        {
+            BoxCollider2D box = obstacle.GetComponent<BoxCollider2D>();
+            if (DetectCollision(playerBody, box))
+            {
+                InCollision = true;
+            }
+        }
+        InCollision = false;
     }
 }
